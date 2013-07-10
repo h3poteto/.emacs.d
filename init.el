c3ce14c634e795a28f94a15944c84eac460824ee
akirafukushima <akira.fukushima@interfirm.co.jp>
2013-07-10 16:39:37 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,223 @@
+;; set load path
+(setq load-path(cons "~/.emacs.d/" load-path))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/auto-install"))
+
+;;for Mac 
+(setq mac-allow-anti-aliasing nil)
+(setq mac-option-modifier 'meta)
+;;(mac-key-mode 1)
+
+;;for Japanese
+(set-language-environment 'Japanese)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'sjis-mac)
+(set-clipboard-coding-system 'sjis-mac)
+(setq-default buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(add-to-list 'default-frame-alist '(font . "fontset-default"))
+
+(show-paren-mode t)
+(column-number-mode t)
+
+;; line number
+(line-number-mode t)
+
+;; line number
+(global-linum-mode)
+
+;; init window
+(setq inhibit-startup-message t)
+
+;; title full path
+(setq frame-title-fomat "%f")
+
+;;; フォントセットを作る
+(let* ((fontset-name "myfonts") ; フォントセットの名前
+       (size 12) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+       (asciifont "Menlo") ; ASCIIフォント
+       (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
+       (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont)) 
+       (fsn (create-fontset-from-ascii-font font nil fontset-name)))
+  (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+  (set-fontset-font fsn '(#x0080 . #x024F) fontspec) ; 分音符付きラテン
+  (set-fontset-font fsn '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
+  )
+ 
+;;; デフォルトのフレームパラメータでフォントセットを指定
+(add-to-list 'default-frame-alist '(font . "fontset-myfonts"))
+ 
+;;; フォントサイズの比を設定
+(dolist (elt '(("^-apple-hiragino.*" . 1.2)
+	       (".*osaka-bold.*" . 1.2)
+	       (".*osaka-medium.*" . 1.2)
+	       (".*courier-bold-.*-mac-roman" . 1.0)
+	       (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+	       (".*monaco-bold-.*-mac-roman" . 0.9)))
+  (add-to-list 'face-font-rescale-alist elt))
+ 
+;;; デフォルトフェイスにフォントセットを設定
+;;; (これは起動時に default-frame-alist に従ったフレームが作成されない現象への対処)
+(set-face-font 'default "fontset-myfonts")
+
+
+;; color setting
+;; set window status
+
+(if window-system (progn
+ (setq initial-frame-alist '((width . 80)(height . 45)(top . 0)(left . 0)))
+ (set-background-color "Black")
+ (set-foreground-color "White")
+ (set-cursor-color "Gray")
+ ))
+
+;; make window transparent
+ (set-frame-parameter nil 'alpha 80)
+
+;; newline and indent
+ (global-set-key "\C-m" 'newline-and-indent)
+
+
+;; auto-install
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
+;(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup) ;install-elisp.el
+
+;; anything
+(global-set-key (kbd "C-x b") 'anything)
+
+;; auto-complete
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+(setq ac-auto-start t)
+
+;; yaml-mode
+(when (require 'yaml-mode nil t)
+  (add-to-list 'auto-mode-alist '("¥¥.yml$" . yaml-mode)))
+
+
+;; rinari
+;; Interactively Do Things (highly recommended, but not strictly required)
+(require 'ido)
+(ido-mode t)
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/rinari")
+(require 'rinari)
+
+;;; rhtml-mode
+(add-to-list 'load-path "~/.emacs.d/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+    (lambda () (rinari-launch)))
+
+;; scss
+(add-to-list 'load-path "~/.emacs.d/scss-mode")
+(require 'scss-mode )
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
+
+;; tabbar
+(add-to-list 'load-path "~/.emacs.d/tabbar")
+;; tabbar.el
+(require 'tabbar)
+(tabbar-mode 1)
+ 
+;; タブ上でマウスホイール操作無効
+(tabbar-mwheel-mode -1)
+ 
+;; グループ化しない
+(setq tabbar-buffer-groups-function nil)
+ 
+;; 左に表示されるボタンを無効化
+(dolist (btn '(tabbar-buffer-home-button
+               tabbar-scroll-left-button
+               tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+                 (cons "" nil))))
+ 
+;; ウインドウからはみ出たタブを省略して表示
+(setq tabbar-auto-scroll-flag nil)
+ 
+;; タブとタブの間の長さ
+(setq tabbar-separator '(1.5))
+ 
+;; 外観変更
+(set-face-attribute
+ 'tabbar-default nil
+ :family "Comic Sans MS"
+ :background "black"
+ :foreground "gray72"
+ :height 1.0)
+(set-face-attribute
+ 'tabbar-unselected nil
+ :background "black"
+ :foreground "grey72"
+ :box nil)
+(set-face-attribute
+ 'tabbar-selected nil
+ :background "black"
+ :foreground "#c82829"
+ :box nil)
+(set-face-attribute
+ 'tabbar-button nil
+ :box nil)
+(set-face-attribute
+ 'tabbar-separator nil
+ :height 1.2)
+ 
+;; タブに表示させるバッファの設定
+(defvar my-tabbar-displayed-buffers
+ '("scratch*" "*Messages*" "*Backtrace*" "*Colors*" "*Faces*" "*vc-")
+  "*Regexps matches buffer names always included tabs.")
+(defun my-tabbar-buffer-list ()
+  "Return the list of buffers to show in tabs.
+Exclude buffers whose name starts with a space or an asterisk.
+The current buffer and buffers matches `my-tabbar-displayed-buffers'
+are always included."
+  (let* ((hides (list ?\  ?\*))
+         (re (regexp-opt my-tabbar-displayed-buffers))
+         (cur-buf (current-buffer))
+         (tabs (delq nil
+                     (mapcar (lambda (buf)
+                               (let ((name (buffer-name buf)))
+                                 (when (or (string-match re name)
+                                           (not (memq (aref name 0) hides)))
+                                   buf)))
+                             (buffer-list)))))
+    ;; Always include the current buffer.
+    (if (memq cur-buf tabs)
+        tabs
+      (cons cur-buf tabs))))
+(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
+ 
+;; タブ移動キーバインド
+(global-set-key (kbd "<M-right>") 'tabbar-forward-tab)
+(global-set-key (kbd "<M-left>") 'tabbar-backward-tab)
+ 
+;; タブモードのオン/オフをトグル
+(global-set-key (kbd "M-4") 'tabbar-mode)
+ 
+(put 'upcase-region 'disabled nil)
+
+
+;; coffee-mode
+(add-to-list 'load-path "~/.emacs.d/coffee-mode")
+(require 'coffee-mode )
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 4)
+ (setq coffee-tab-width 4))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+
+;;コメントアウト
+(define-key global-map(kbd "C-c /") 'comment-or-uncomment-region)
