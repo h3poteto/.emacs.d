be0ba6cce002ed83c6feb163ecb01ddb3477497f
akirafukushima <akira.fukushima@interfirm.co.jp>
2013-10-03 17:48:07 +0900

    add file-info

--- init.el
+++ init.el
@@ -90,6 +90,7 @@
 (add-hook 'rhtml-mode-hook
     (lambda () (rinari-launch)))
 (add-to-list 'auto-mode-alist '("\\.ctp\\'" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.text.erb\\'" . rhtml-mode))
 
 ;; scss
 (require 'scss-mode )
@@ -227,3 +228,30 @@ are always included."
 
 ;; grep-edit
 (require 'grep-edit)
+
+;;file-info
+(defun file-info () "
+カレントバッファのファイル情報を表示する。"
+  (interactive)
+  (if (buffer-file-name (current-buffer))
+      (progn
+        (let* ((file-name (buffer-file-name (current-buffer)))
+               (f-attr (file-attributes file-name))
+               (a-time (nth 4 f-attr))  ; 最終アクセス時刻
+               (m-time (nth 5 f-attr))  ; 最終修正時刻
+               (f-size (nth 7 f-attr))  ; ファイルサイズ
+               (f-mode (nth 8 f-attr))  ; ファイル属性
+               (mes1 (format "ファイルパス:   %s\n" file-name))
+               (mes2 (format "最終参照時刻:   %s\n"
+                              (format-time-string
+                               "%Y-%m-%d %H:%M:%S" a-time)))
+               (mes3 (format "最終修正時刻:   %s\n"
+                              (format-time-string
+                               "%Y-%m-%d %H:%M:%S" m-time)))
+               (mes4 (format "ファイルサイズ: %s byte\n" f-size))
+               (mes5 (format "ファイル属性:   %s" f-mode))
+               (mess (concat mes1 mes2 mes3 mes4 mes5)))
+          (message "%s" mess)))
+    nil ))
+
+(global-set-key "\C-ci" 'file-info) 
