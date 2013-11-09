9b7559f5abba8500e4042917e0f4b6ccd7b9c4e4
akirafukushima <akira.fukushima@interfirm.co.jp>
2013-11-09 22:46:04 +0900

    add tramp command

--- init.el
+++ init.el
@@ -255,3 +255,12 @@ are always included."
     nil ))
 
 (global-set-key "\C-ci" 'file-info) 
+
+;; tramp
+(defun reopen-with-sudo ()
+  "Reopen current buffer-file with sudo using tramp."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if file-name
+        (find-alternate-file (concat "/sudo::" file-name))
+      (error "Cannot get a file name"))))
