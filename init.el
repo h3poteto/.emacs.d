b095e3b4e691800049728ae30f88f8fc0466d496
akirafukushima <akira.fukushima@interfirm.co.jp>
2015-01-29 13:24:12 +0900

    highlighting current line

--- init.el
+++ init.el
@@ -375,3 +375,23 @@ are always included."
 ;; highlight-indentation
 (require 'highlight-indentation)
 (setq highlight-indentation-offset 2)
+(set-face-background 'highlight-indentation-face "#202020")
+(set-face-background 'highlight-indentation-current-column-face "#303030")
+
+(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
+(add-hook 'highlight-indentation-mode-hook 'highlight-indentation-current-column-mode)
+
+;; カーソル行をハイライト
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "gray20"))
+    (((class color)
+      (background light))
+     (:background "ForestGreen"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+;; (setq hl-line-face 'underline) ; 下線
+(global-hl-line-mode)
