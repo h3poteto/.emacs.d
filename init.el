d41f20c332119bca36ed5dd960998572aabf0768
h3poteto <h3.poteto@gmail.com>
2015-04-13 01:44:32 +0900

    fix deep indent paren for enh

--- init.el
+++ init.el
@@ -391,21 +391,22 @@ are always included."
 (add-hook 'enh-ruby-mode-hook '(lambda () (ruby-electric-mode t)))
 
 ;; ruby settings
-(setq ruby-deep-indent-paren-style nil)
-(defadvice ruby-indent-line (after unindent-closing-paren activate)
-  (let ((column (current-column))
-        indent offset)
-    (save-excursion
-      (back-to-indentation)
-      (let ((state (syntax-ppss)))
-        (setq offset (- column (current-column)))
-        (when (and (eq (char-after) ?\))
-                   (not (zerop (car state))))
-          (goto-char (cadr state))
-          (setq indent (current-indentation)))))
-    (when indent
-      (indent-line-to indent)
-      (when (> offset 0) (forward-char offset)))))
+(setq enh-ruby-deep-indent-paren nil)
+;; (setq ruby-deep-indent-paren-style nil)
+;; (defadvice ruby-indent-line (after unindent-closing-paren activate)
+;;   (let ((column (current-column))
+;;         indent offset)
+;;     (save-excursion
+;;       (back-to-indentation)
+;;       (let ((state (syntax-ppss)))
+;;         (setq offset (- column (current-column)))
+;;         (when (and (eq (char-after) ?\))
+;;                    (not (zerop (car state))))
+;;           (goto-char (cadr state))
+;;           (setq indent (current-indentation)))))
+;;     (when indent
+;;       (indent-line-to indent)
+;;       (when (> offset 0) (forward-char offset)))))
 
 ;; enh-ruby
 ;; 保存時にmagic commentを追加しないようにする
