82a729f0213d215747218ebcb73ca39e2b8dde3d
h3poteto <h3.poteto@gmail.com>
2015-08-25 00:12:13 +0900

    chenge flycheckers for javascript

--- init.el
+++ init.el
@@ -503,4 +503,7 @@ are always included."
 (require 'flycheck-color-mode-line)
 
 (eval-after-load "flycheck"
-  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+  '(progn
+     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
+     (setq flycheck-disabled-checkers '(javascirpt-jshint))
+     (setq flycheck-checkers '(javascript-eslint))))
