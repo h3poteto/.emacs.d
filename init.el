1a68a50a9897595ea2785312f6156404949a70e0
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2015-09-03 12:09:10 +0900

    add custom revert-buffer

--- init.el
+++ init.el
@@ -250,6 +250,20 @@ are always included."
 ;; find
 (global-set-key "\C-x\C-f" 'find-name-dired)
 
+;; revert-buffer
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  ;;(message "force-reverting value is %s" force-reverting)
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
+
+;; reload buffer
+(global-set-key (kbd "<f5>") 'revert-buffer-no-confirm)
 
 ;; タブ、全角スペースのハイライト
 (defface my-face-r-1 '((t (:background "gray15"))) nil)
