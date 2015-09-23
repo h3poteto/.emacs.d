dd290e3eb952e3b14fe0174f661b85f9c0817ece
h3poteto <h3.poteto@gmail.com>
2015-09-23 16:24:20 +0900

    update web-mode for jsx

--- init.el
+++ init.el
@@ -547,6 +547,17 @@ are always included."
 ;; web-mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 
 ;;;; check
 
