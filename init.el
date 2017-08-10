977786eb7c196bee25f97214349c29b4f512e3d9
AkiraFukushima <h3.poteto@gmail.com>
2017-08-10 13:59:12 +0900

    Remove invisible file in fiplir ignore files

--- init.el
+++ init.el
@@ -469,7 +469,7 @@ are always included."
 (require 'fiplr)
 (setq fiplr-root-markers '(".git" ".svn"))
 (setq fiplr-ignored-globs '((directories (".git" ".svn" "vendor" "tmp" "uploaded_files" "log" "./public" "node_modules" "_vendor" "coverage" "_build" "deps"))
-                            (files ("*~" ".*" "*.jpg" "*.png" "*.gif" "*.zip" "*.DS_Store"))))
+                            (files ("*~" "*.jpg" "*.png" "*.gif" "*.zip" "*.DS_Store"))))
 (global-set-key (kbd "C-x f") 'fiplr-find-file)
 (global-set-key (kbd "C-x c") 'fiplr-clear-cache)
 
