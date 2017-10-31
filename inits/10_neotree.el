f25cc3bd029c1626177c9b5a093f9b0f5a1fc525
AkiraFukushima <h3.poteto@gmail.com>
2017-11-01 00:25:36 +0900

    Rewrite neotree using use-package

--- inits/10_neotree.el
+++ inits/10_neotree.el
@@ -1,12 +1,11 @@
 ;; neotree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-;; 隠しファイルをデフォルトで表示
-(setq-default neo-show-hidden-files t)
-(define-key global-map(kbd "C-c n") 'neotree-hidden-file-toggle)
-;; neotree でファイルを新規作成した後、自動的にファイルを開く
-(setq neo-create-file-auto-open t)
-;; キーバインドをシンプルにする
-(setq neo-keymap-style 'concise)
-;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
-(setq neo-smart-open t)
+(use-package neotree
+  :bind
+  (("<f8>" . neotree-toggle)
+   ("C-c n" . neotree-hidden-file-toggle))
+  :config
+  ;; 隠しファイルをデフォルトで表示
+  (setq-default neo-show-hidden-files t)
+  (setq neo-create-file-auto-open t)
+  (setq neo-keymap-style 'concise)
+  (setq neo-smart-open t))
