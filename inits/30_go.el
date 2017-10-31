23d8f73775f9880486fd61347caefba5598d7bf8
AkiraFukushima <h3.poteto@gmail.com>
2017-10-31 22:48:29 +0900

    Revert "Rewrite go-mode using use-package"
    This reverts commit 63a26a39d879efda715b5a90cd622efcbbbc931f.

--- inits/30_go.el
+++ inits/30_go.el
@@ -1,14 +1,13 @@
 ;; go-mode
 ;; this mode demand gocode, godef and godoc in $GOPATH
-(use-package go-mode
-  :ensure t
-  :bind
-  (("M-." . godef-jump)
-   ("C-c d" . godoc))
-  :config
-  (progn (require 'go-autocomplete))
-  (add-hook 'go-mode-hook 'go-eldoc-setup)
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq c-basic-offset 4)
-  (setq indent-tabs-mode t))
+(eval-after-load "go-mode" '(progn (require 'go-autocomplete)))
+(add-hook 'go-mode-hook
+          '(lambda()
+             (setq c-basic-offset 4)
+             (setq indent-tabs-mode t)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "C-c d") 'godoc)
+             (go-eldoc-setup)
+             (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)))
+(add-hook 'before-save-hook 'gofmt-before-save)
 
