e25bd199ba9e1f986e34676b3cf2cb6ad291456a
AkiraFukushima <h3.poteto@gmail.com>
2017-11-01 00:52:56 +0900

    Rewrite ag using use-package

--- inits/10_ag.el
+++ inits/10_ag.el
@@ -1,25 +1,34 @@
-; ag
-;;(setq default-process-coding-system 'utf-8-unix)  ; ag 検索結果のエンコード指定
-(require 'ag)
-(setq ag-highlight-search t)  ; 検索キーワードをハイライト
-(setq ag-reuse-buffers t)     ; 検索用バッファを使い回す (検索ごとに新バッファを作らない)
+;; ag
+;; ag 検索結果のエンコード指定
+;;(setq default-process-coding-system 'utf-8-unix)
 
-; helm
-; helm-ag
-(require 'helm-config)
-(require 'helm-files)
-(require 'helm-ag)
-(global-set-key (kbd "M-g .") 'helm-ag)
-(global-set-key (kbd "M-g r") 'helm-ag-project-root)
-(global-set-key (kbd "M-g ,") 'helm-ag-pop-stack)
-(custom-set-variables
- '(helm-ag-use-grep-ignore-list "*~")
- '(helm-ag-use-agignore t)
- )
+(use-package ag
+  :config
+  ;; 検索結果のハイライト
+  (setq ag-highlight-search t)
+  ;; 検索用バッファを使いまわす
+  (setq ag-reuse-buffers t)
+  (add-hook 'ag-mode-hook 'wgrep-ag-setup))
 
-; wgrep
-(add-hook 'ag-mode-hook '(lambda ()
-                           (require 'wgrep-ag)
-                           (setq wgrep-auto-save-buffer t)  ; 編集完了と同時に保存
-                           (setq wgrep-enable-key "r")      ; "r" キーで編集モードに
-                           (wgrep-ag-setup)))
+;; helm
+(use-package helm-config)
+(use-package helm-files)
+
+;; helm-ag
+(use-package heml-ag
+  :bind
+  (("M-g ." . helm-ag)
+   ("M-g r" . helm-ag-project-root)
+   ("M-g /" . helm-ag-pop-stack))
+  :config
+  (custom-set-variables
+   '(helm-ag-use-grep-ignore-list "*~")
+   '(helm-ag-use-agignore t)
+   ))
+
+;; wgrep-ag
+(use-package wgrep-ag
+  :after ag
+  :config
+  (setq wgrep-auto-save-buffer t)  ; 編集完了と同時に保存
+  (setq wgrep-enable-key "r"))      ; "r" キーで編集モードに
