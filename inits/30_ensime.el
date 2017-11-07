37205732647589bcbf61ecc262f2a72bfd5e4304
AkiraFukushima <h3.poteto@gmail.com>
2017-11-07 16:44:22 +0900

    Define scalafmt function for scalafmt

--- inits/30_ensime.el
+++ inits/30_ensime.el
@@ -1,3 +1,40 @@
 ;; ensime
-(add-hook 'before-save-hook 'ensime-format-source)
+;; smartparens
 (add-hook 'scala-mode-hook #'smartparens-mode)
+
+;; for scalariform in ensime
+;;(add-hook 'before-save-hook 'ensime-format-source)
+
+(add-hook 'after-save-hook 'scalafmt-after-save)
+
+;; for scalafmt in brew command
+(defcustom scalafmt-command "scalafmt"
+  "The scalafmt command."
+  :type 'string)
+(defcustom scalafmt-args nil
+  "Additional arguments to pass to scalafmt."
+  :type '(list string))
+
+(defun scalafmt ()
+  (interactive)
+  (setq errbuf "*Scalafmt Errors*")
+  (let (our-scalafmt-args)
+    (get-buffer-create errbuf)
+
+    (setq our-scalafmt-args
+          (append scalafmt-args
+                  (list (file-truename buffer-file-name))))
+    ;; 現在のバッファに対してfmtしたい場合は以下を使う
+    ;; しかし渡し方に問題があり動作しない
+    ;; (setq our-scalafmt-args
+    ;;       (append scalafmt-args
+    ;;               (list "--stdin" (buffer-string))))
+    (message "Calling scalafmt: %s %s" scalafmt-command (mapconcat 'identity our-scalafmt-args " "))
+    (apply #'call-process scalafmt-command nil errbuf nil our-scalafmt-args)
+    (if (equal (buffer-size (get-buffer errbuf)) 0)
+        (kill-buffer errbuf))
+    (revert-buffer-no-confirm)))
+
+(defun scalafmt-after-save ()
+  (interactive)
+  (when (eq major-mode 'scala-mode) (scalafmt)))
