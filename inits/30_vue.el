c7bab008f7624f26e03b1b836480b18c3376c1cf
AkiraFukushima <h3.poteto@gmail.com>
2018-07-10 00:29:35 +0900

    Apply js-switch-indent-offset at vue-mode

--- inits/30_vue.el
+++ inits/30_vue.el
@@ -2,5 +2,6 @@
 ;; 内部でcss-modeを使用するときにオフセットを変更したい
 (add-hook 'vue-mode-hook
           (lambda ()
-            (setq css-indent-offset 2)))
+            (setq css-indent-offset 2)
+            (setq js-switch-indent-offset 2)))
 (add-hook 'vue-mode-hook 'rainbow-mode)
