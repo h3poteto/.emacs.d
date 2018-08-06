37e930be26a5ff049059d28bcf788a6d1aaaadc5
AkiraFukushima <h3.poteto@gmail.com>
2018-08-06 16:32:19 +0900

    Fix flymake error popup for emacs 26

--- inits/40_flymake.el
+++ inits/40_flymake.el
@@ -1,11 +1,7 @@
-;; 現在行のエラーを表示する
-(global-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
+;; emacs 26ではflymake-popup-current-error-menuが提供されていない
+
+;; ;; 現在行のエラーを表示する
+;; (global-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
 
 ;; 次のエラーまでジャンプ
-(defun next-flymake-error ()
-  (interactive)
-  (flymake-goto-next-error)
-  (let ((err (get-char-property (point) 'help-echo)))
-    (when err
-      (message err))))
-(global-set-key (kbd "C-c e") 'next-flymake-error)
+(global-set-key (kbd "C-c e") 'flymake-goto-next-error)
