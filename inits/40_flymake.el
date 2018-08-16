fe0f7f751f06ad210e80a6414e4f76e01420c891
AkiraFukushima <h3.poteto@gmail.com>
2018-08-16 21:19:57 +0900

    Revert "Fix flymake error popup for emacs 26"
    This reverts commit 37e930be26a5ff049059d28bcf788a6d1aaaadc5.

--- inits/40_flymake.el
+++ inits/40_flymake.el
@@ -1,7 +1,11 @@
-;; emacs 26ではflymake-popup-current-error-menuが提供されていない
-
-;; ;; 現在行のエラーを表示する
-;; (global-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
+;; 現在行のエラーを表示する
+(global-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
 
 ;; 次のエラーまでジャンプ
-(global-set-key (kbd "C-c e") 'flymake-goto-next-error)
+(defun next-flymake-error ()
+  (interactive)
+  (flymake-goto-next-error)
+  (let ((err (get-char-property (point) 'help-echo)))
+    (when err
+      (message err))))
+(global-set-key (kbd "C-c e") 'next-flymake-error)
