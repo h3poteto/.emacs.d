8a5d6172126d23761e1125198493ad70b823e993
AkiraFukushima <h3.poteto@gmail.com>
2019-03-12 23:51:07 +0900

    Set indent level for js in vue

--- inits/30_vue.el
+++ inits/30_vue.el
@@ -3,6 +3,8 @@
   :config
   (setq css-indent-offset 2)
   (setq js-switch-indent-offset 2)
+  (setq js-indent-level 2)
+  (setq js2-basic-offset 2)
   (setq mmm-submode-decoration-level 0)
   :hook
   (vue-mode . prettier-js-mode))
