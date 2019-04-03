ae85a7500846cdbdf8cb5df4b249aa0a8ffa7f4c
AkiraFukushima <h3.poteto@gmail.com>
2019-04-03 23:44:46 +0900

    Disable tide which is formatter for typescript

--- inits/30_typescript.el
+++ inits/30_typescript.el
@@ -2,12 +2,12 @@
   :mode
   ("\\.ts\\'" . typescript-mode)
   :hook
-  (before-save . tide-format-before-save)
+  ;; (before-save . tide-format-before-save)
   (typescript-mode . (lambda ()
                        (setq typescript-indent-level 2)
                        (tide-setup)
                        (flycheck-mode +1)
-                       (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                       ;;(setq flycheck-check-syntax-automatically '(save mode-enabled))
                        (eldoc-mode +1)
                        (tide-hl-identifier-mode +1)
                        ;; company is an optional dependency. You have to
