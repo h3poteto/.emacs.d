620c52988beb1cce7827364cb2d36ab6bf092a17
AkiraFukushima <h3.poteto@gmail.com>
2019-04-11 13:23:55 +0900

    Change indent mode in protobuf-mode

---
+++ inits/30_protobuf.el
@@ -0,0 +1,9 @@
+(use-package protobuf-mode
+  :mode
+  ("\\.proto$" . protobuf-mode)
+  :config
+  (defconst my-protobuf-style
+    '((c-basic-offset . 2)
+      (indent-tabs-mode . nil)))
+  :hook
+  (protobuf-mode . (lambda () (c-add-style "my-style" my-protobuf-style t))))
