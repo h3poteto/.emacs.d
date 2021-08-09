991fe65d7f2347c2f142240af7ec3ea86413107a
AkiraFukushima <h3.poteto@gmail.com>
2021-08-09 21:29:23 +0900

    Add ignore sample file

---
+++ packages/ignore.sample.el
@@ -0,0 +1,27 @@
+;; These minor-modes will be ignored when load.
+;; If you want to ignore a minor-mode, please set it to true.
+;; For example,
+;;
+;; (setq ignore-go-mode t)
+;;
+;; If you do not want to ignore,
+;;
+;; (setq ignore-go-mode nil)
+
+;; (setq ignore-css-mode t)
+;; (setq ignore-csv-mode t)
+;; (setq ignore-dockerfile-mode t)
+;; (setq ignore-elixir-mode t)
+;; (setq ignore-go-mode t)
+;; (setq ignore-js-mode t)
+;; (setq ignore-json-mode t)
+;; (setq ignore-markdown-mode t)
+;; (setq ignore-php-mode t)
+;; (setq ignore-protobuf-mode t)
+;; (setq ignore-python-mode t)
+;; (setq ignore-rust-mode t)
+;; (setq ignore-terraform-mode t)
+;; (setq ignore-vue-mode t)
+;; (setq ignore-web-mode t)
+;; (setq ignore-ruby-mode t)
+;; (setq ignore-coffee-mode t)
