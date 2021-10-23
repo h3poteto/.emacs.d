3444c16a7082ce2c5c7411af6e62626f4ccae17a
AkiraFukushima <h3.poteto@gmail.com>
2021-10-23 21:48:53 +0900

    Update dap for elixir-mode

--- inits/30_elixir.el
+++ inits/30_elixir.el
@@ -6,6 +6,24 @@
   (add-hook 'elixir-mode-hook
             (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
   (setq lsp-elixir-dialyzer-enabled nil)
+  (defhydra hydra-elixir-mode (:hint nil :exit t)
+"
+^Main^
+^^^^^^-----------------------------------------------------
+_eb_: elixir-beginning-of-defun
+_ee_: elixir-end-of-defun
+_ed_: elixir-mode-open-docs-master
+"
+
+("eb" elixir-beginning-of-defun)
+("ee" elixir-end-of-defun)
+("ed" elixir-mode-open-docs-master)
+
+("z" nil "leave"))
+
+  :bind
+  (:map elixir-mode-map
+   ("C-z" . hydra-elixir-mode/body))
   :hook
   (elixir-mode . (lambda ()
                    (hs-minor-mode 1)))
